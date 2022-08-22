fc31f234c7f90d060dd8cbf74661f842db1326f6
kiniaak <88184781+kiniaak@users.noreply.github.com>
2022-08-22 08:31:08 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+import React from 'react';
+import BackgroundAnimate from './BackgroundAnimate';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
+
+function App() {
+  const [inputValue,setInputValue]=useState("");
+
+
+  return (
+    <div className="container">
+     
+          <InputShortener setInputValue={setInputValue}/>
+          <BackgroundAnimate/>
+          <LinkResult inputValue={inputValue}/>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BackgroundAnimate.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const BackgroundAnimate = () => {
+    return (
+        <div className="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+        
+    )
+}
+export default BackgroundAnimate
